ba48af52394acc0df98dd5513a5c586b675951d8
kapselccc <kapselccc@wp.pl>
2023-04-23 20:42:08 +0200

    added getMinMaxExchangeRate

--- src/main/java/pl/kpc/npb_api/exchanges/ExchangeService.java
+++ src/main/java/pl/kpc/npb_api/exchanges/ExchangeService.java
@@ -1,10 +1,8 @@
 package pl.kpc.npb_api.exchanges;
 
-import com.fasterxml.jackson.core.JsonParser;
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.json.simple.JSONValue;
-import org.json.simple.parser.JSONParser;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
@@ -12,8 +10,7 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.util.NoSuchElementException;
-import java.util.Optional;
+import java.util.*;
 
 
 @Service
@@ -21,8 +18,58 @@ public class ExchangeService {
 
 
     public double getAvgExchangeRateOnDate(String date, String code) throws NoSuchElementException {
+        String response = sendGetRequest(String.format("http://api.nbp.pl/api/exchangerates/rates/a/%s/%s/",
+                code,date));
+        return getAvgExchangeRateFromJson(response).orElseThrow();
+    }
+
+    public List<Double> getMinMaxExchangeRate(String code, int n){
+        if(n > 255)
+        {
+            throw new IllegalArgumentException("N must be <= 255");
+        }else if( n <= 0){
+            throw new IllegalArgumentException("N must be positive number <= 255");
+        }
+        String response = sendGetRequest(String.format("http://api.nbp.pl/api/exchangerates/rates/a/%s/last/%d/",
+                code, n));
+
+        List<Double> list = getManyExchangeRateFromJson(response);
+        Collections.sort(list);
+        return new ArrayList<>(List.of(list.get(0),list.get(list.size() - 1) ));
+    }
+
+    private Optional<Double> getAvgExchangeRateFromJson(String s){
+        Double mid = null;
+        try {
+            JSONObject jo = new JSONObject(s);
+            JSONArray ratesArray = (JSONArray) jo.get("rates");
+            JSONObject ratesObject = (JSONObject) ratesArray.get(0);
+            mid = (Double) ratesObject.get("mid");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return Optional.ofNullable(mid);
+    }
+
+    private List<Double> getManyExchangeRateFromJson(String s){
+        List<Double> list = new ArrayList<>();
+        try {
+            JSONObject jo = new JSONObject(s);
+            JSONArray ratesArray = (JSONArray) jo.get("rates");
+            for(int i = 0;i < ratesArray.length();i++){
+                JSONObject ratesObject = (JSONObject) ratesArray.get(i);
+                list.add((Double) ratesObject.get("mid"));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
+
+    private String sendGetRequest(String uri){
         HttpRequest request = HttpRequest.newBuilder(
-                        URI.create(String.format("http://api.nbp.pl/api/exchangerates/rates/a/%s/%s/",code,date)))
+                        URI.create(uri))
                 .header("accept","application/json" )
                 .method("GET", HttpRequest.BodyPublishers.noBody())
                 .build();
@@ -34,20 +81,7 @@ public class ExchangeService {
             exception.printStackTrace();
         }
 
-        return getAvgExchangeRateFromJson(response.body()).orElseThrow();
+        return response.body();
     }
 
-    public Optional<Double> getAvgExchangeRateFromJson(String s){
-        Double mid = null;
-        try {
-            JSONObject jo = (JSONObject) JSONValue.parse(s);
-            JSONArray ratesArray = (JSONArray) jo.get("rates");
-            JSONObject ratesObject = (JSONObject) ratesArray.get(0);
-            mid = (Double) ratesObject.get("mid");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        return Optional.ofNullable(mid);
-    }
 }
--- src/test/java/pl/kpc/npb_api/exchanges/ExchangeServiceTest.java
+++ src/test/java/pl/kpc/npb_api/exchanges/ExchangeServiceTest.java
@@ -4,7 +4,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.Objects;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
@@ -16,20 +19,6 @@ class ExchangeServiceTest {
     private ExchangeService service;
 
     @Test
-    void getAvgExchangeRateFromJsonTest(){
-        String json = "{\"table\":\"A\",\"currency\":\"euro\",\"code\":\"EUR\",\"rates\":[{\"no\":\"1/A/NBP/2012\"," +
-            "\"effectiveDate\":\"2012-01-02\",\"mid\":4.4640}]}";
-        assertEquals(4.464,service.getAvgExchangeRateFromJson(json).get());
-    }
-
-    @Test
-    void getAvgExchangeRateFromJsonTestNull(){
-        String json = "{random string}";
-        assertEquals(null,service.getAvgExchangeRateFromJson(json).orElse(null));
-    }
-
-
-    @Test
     void getAvgExchangeRateOnDateTest(){
         assertThat(service.getAvgExchangeRateOnDate("2012-01-02", "eur") == 4.464);
     }
@@ -39,5 +28,16 @@ class ExchangeServiceTest {
         assertThrows(NoSuchElementException.class, () -> {
             service.getAvgExchangeRateOnDate("2012-01-02", "pln");
         });
+    }
+
+    @Test
+    void getMinMaxExchangeRateTest() {
+        // The min, max values should be updated to the latest values
+        Double min = 4.6039;
+        Double max = 4.6278;
+        List<Double> result = service.getMinMaxExchangeRate("eur", 3);
+        assertEquals(result.get(0), min);
+        assertEquals(result.get(1), max);
+
     }
 }
